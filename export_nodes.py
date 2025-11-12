0f74b65dd8dee8c53f268c52dbba19d09cb1a017
Vollkornaffe <vollkornaffe@gmail.com>
2025-11-12 02:09:34 +0100

    nothing much

--- export_nodes.py
+++ export_nodes.py
@@ -17,8 +17,67 @@ def export_node_socket(socket: bpy.types.NodeSocket):
     for m in COPY_MEMBERS_NODE_SOCKET:
         d[m] = getattr(socket, m)
 
+    # this will be useful for setting the defaults
     d["identifier"] = socket.identifier
+
+    # only needed for recreating the socket, possibly useful in custom nodes?
     d["use_multi_input"] = socket.is_multi_input
+
+    # all of these can just be stored in JSON
+    if socket.type in [
+        "VALUE",
+        "INT",
+        "BOOLEAN",
+        "STRING",
+        "MENU",  # this also ends up being a string
+    ]:
+        d["default_value"] = socket.default_value
+
+    # both of these work by with a 3-float-array
+    if socket.type in [
+        "VECTOR",
+        "ROTATION",  # always XYZ ordering
+    ]:
+        d["default_value"] = [
+            socket.default_value[0],
+            socket.default_value[1],
+            socket.default_value[2],
+        ]
+
+    # here it's 4 floats
+    if socket.type == "RGBA":
+        d["default_value"] = [
+            socket.default_value[0],
+            socket.default_value[1],
+            socket.default_value[2],
+            socket.default_value[3],
+        ]
+
+    # best we can do here is record the name?
+    if socket.type in [
+        "OBJECT",
+        "MATERIAL",
+        "TEXTURE",
+        "IMAGE",
+        "COLLECTION",
+    ]:
+        d["default_value"] = (
+            None if socket.default_value is None else socket.default_value.name
+        )
+
+    # seems (currently) not possible to set a default value
+    if socket.type in [
+        "MATRIX",
+        "SHADER",
+        "BUNDLE",
+        "CLOSURE",
+        "GEOMETRY",
+    ]:
+        pass
+
+    # the group input has virtual sockets that fall into here
+    if socket.type == "CUSTOM":
+        pass
 
     return d
 
@@ -29,6 +88,7 @@ def export_node_link(link: bpy.types.NodeLink):
     for m in COPY_MEMBERS_NODE_LINK:
         d[m] = getattr(link, m)
 
+    # link in second pass
     d["from_node"] = link.from_node.name
     d["from_socket"] = link.from_socket.identifier
     d["to_node"] = link.to_node.name
@@ -43,22 +103,16 @@ def export_node(node: bpy.types.Node):
     for m in COPY_MEMBERS_NODE:
         d[m] = getattr(node, m)
 
-    # TODO
-    color = {}
-    location = ()
-    location_absolute = ()
-    parent = "TODO"
+    # these are just a bit special
+    d["color"] = [node.color.r, node.color.g, node.color.b]
+    d["location"] = [node.location.x, node.location.y]
+    d["location_absolute"] = [node.location_absolute.x, node.location_absolute.y]
 
-    # TODO: is this needed for custom nodes, maybe?
-    inputs = [export_node_socket(socket) for socket in node.inputs]
-    outputs = [export_node_socket(socket) for socket in node.outputs]
+    # use this in the second pass
+    d["parent"] = None if node.parent is None else node.parent.name
 
-    d["color"] = color
-    d["location"] = location
-    d["location_absolute"] = location_absolute
-    d["parent"] = parent
-    d["inputs"] = inputs
-    d["outputs"] = outputs
+    d["inputs"] = [export_node_socket(socket) for socket in node.inputs]
+    d["outputs"] = [export_node_socket(socket) for socket in node.outputs]
 
     return d
 
@@ -75,13 +129,9 @@ def export_node_tree(node_tree: bpy.types.NodeTree):
     # TODO: grease_pencil
 
     # TODO
-    interface = {}
-    links = [export_node_link(link) for link in node_tree.links]
-    nodes = [export_node(node) for node in node_tree.nodes]
-
-    d["interface"] = interface
-    d["links"] = links
-    d["nodes"] = nodes
+    d["interface"] = {}
+    d["links"] = [export_node_link(link) for link in node_tree.links]
+    d["nodes"] = [export_node(node) for node in node_tree.nodes]
 
     return d
 
