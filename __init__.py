ea4a19c04973205f5101386e454fd60162247ef7
Vollkornaffe <vollkornaffe@gmail.com>
2025-11-21 01:28:01 +0100

    immediately clear cache and never expose it to the user

--- __init__.py
+++ __init__.py
@@ -16,10 +16,8 @@ import bpy
 from .ui import (
     Tree_Clipper_External_Item,
     SCENE_UL_Tree_Clipper_External_List,
-    SCENE_OT_Tree_Clipper_Export_Clear_Cache,
     SCENE_OT_Tree_Clipper_Export_Cache,
     SCENE_OT_Tree_Clipper_Export_Prepare,
-    SCENE_OT_Tree_Clipper_Import_Clear_Cache,
     SCENE_OT_Tree_Clipper_Import_Cache,
     SCENE_OT_Tree_Clipper_Import_Prepare,
     SCENE_PT_Tree_Clipper_Panel,
@@ -29,10 +27,8 @@ classes = [
     Tree_Clipper_External_Item,
     SCENE_UL_Tree_Clipper_External_List,
     SCENE_OT_Tree_Clipper_Export_Cache,
-    SCENE_OT_Tree_Clipper_Export_Clear_Cache,
     SCENE_OT_Tree_Clipper_Export_Prepare,
     SCENE_OT_Tree_Clipper_Import_Cache,
-    SCENE_OT_Tree_Clipper_Import_Clear_Cache,
     SCENE_OT_Tree_Clipper_Import_Prepare,
     SCENE_PT_Tree_Clipper_Panel,
 ]
