48ada10fa065e71c6a60c0295ce55a55f7b87e2d
Vollkornaffe <vollkornaffe@gmail.com>
2025-12-11 15:17:33 +0100

    improve debuggability

--- tree_clipper/id_data_getter.py
+++ tree_clipper/id_data_getter.py
@@ -71,4 +71,9 @@ def canonical_reference(obj: bpy.types.bpy_struct) -> bpy.types.bpy_struct:
 
     data_block = _ID_TYPE_TO_DATA_BLOCK[obj.id_type]()
 
-    return next(ref for ref in data_block if ref.name == obj.name)
+    ref = next((ref for ref in data_block if ref.name == obj.name), None)
+
+    # this is easier to debug than StopIteration
+    assert ref is not None
+
+    return ref
