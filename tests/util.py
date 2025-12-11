530c0fdf751d0c31b29d38b65784a1e6105e68eb
Vollkornaffe <vollkornaffe@gmail.com>
2025-12-11 18:07:45 +0100

    add util to roundtrip with external

--- tests/util.py
+++ tests/util.py
@@ -2,6 +2,12 @@ from pathlib import Path
 from typing import Literal
 import bpy
 
+from tree_clipper.common import (
+    EXTERNAL_FIXED_TYPE_NAME,
+    EXTERNAL_DESCRIPTION,
+    EXTERNAL_SERIALIZATION,
+)
+from tree_clipper.id_data_getter import get_data_block_from_id_name
 from tree_clipper.export_nodes import ExportIntermediate, ExportParameters
 from tree_clipper.import_nodes import ImportIntermediate, ImportParameters
 from tree_clipper.specific_handlers import BUILT_IN_EXPORTER, BUILT_IN_IMPORTER
@@ -63,6 +69,55 @@ def round_trip_without_external(name: str):
 
     import_intermediate = ImportIntermediate()
     import_intermediate.from_str(string)
+    import_intermediate.import_nodes(
+        parameters=ImportParameters(
+            specific_handlers=BUILT_IN_IMPORTER,
+            allow_version_mismatch=False,
+            overwrite=True,
+            debug_prints=True,
+        )
+    )
+
+
+def round_trip_with_same_external(name: str, is_material: bool):
+    export_intermediate = ExportIntermediate(
+        parameters=ExportParameters(
+            is_material=is_material,
+            name=name,
+            specific_handlers=BUILT_IN_EXPORTER,
+            export_sub_trees=True,
+            skip_defaults=True,
+            debug_prints=True,
+            write_from_roots=False,
+        )
+    )
+
+    export_intermediate.set_external(
+        (
+            external_id,
+            external_item.pointed_to_by.get_pointee().name,  # ty: ignore[possibly-missing-attribute]
+        )
+        for external_id, external_item in export_intermediate.get_external().items()
+    )
+
+    string = export_intermediate.export_to_str(compress=False, json_indent=4)
+    print(string)
+
+    import_intermediate = ImportIntermediate()
+    import_intermediate.from_str(string)
+
+    def get_same_external_item(external_item: EXTERNAL_SERIALIZATION):
+        fixed_type_name = external_item[EXTERNAL_FIXED_TYPE_NAME]
+        assert isinstance(fixed_type_name, str)
+        data_block = get_data_block_from_id_name(fixed_type_name)
+        name = external_item[EXTERNAL_DESCRIPTION]
+        return data_block[name]  # ty: ignore[invalid-argument-type]
+
+    import_intermediate.set_external(
+        (int(external_id), get_same_external_item(external_item))
+        for external_id, external_item in import_intermediate.get_external().items()
+    )
+
     import_intermediate.import_nodes(
         parameters=ImportParameters(
             specific_handlers=BUILT_IN_IMPORTER,
