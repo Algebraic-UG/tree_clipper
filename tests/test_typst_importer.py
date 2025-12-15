71e1b20061aca1a0a4dc7edeb2fc95daf865a07f
Vollkornaffe <vollkornaffe@gmail.com>
2025-12-15 14:24:04 +0100

    add test for typst importer

---
+++ tests/test_typst_importer.py
@@ -0,0 +1,26 @@
+import bpy
+
+from pathlib import Path
+
+from .util import save_failed, round_trip_with_same_external
+
+
+_DIR = Path("tests") / "binary_blend_files" / "typst_importer"
+
+
+def test_blender_assets():
+    path = _DIR / "blender_assets.blend"
+    try:
+        bpy.ops.wm.open_mainfile(filepath=str(path))
+
+        round_trip_with_same_external(name="FONT_FILL", is_material=False)
+        round_trip_with_same_external(name="JUMP", is_material=False)
+
+        round_trip_with_same_external(name="GPFILL", is_material=True)
+        # doesn't have a node tree?
+        # round_trip_with_same_external(name="GPStroke", is_material=True)
+
+    except:
+        # store in case of failure for easy debugging
+        save_failed(f"{test_blender_assets.__name__}")
+        raise
