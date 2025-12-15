e31cf19068d494ff381629985f914f2aea1f09a4
Vollkornaffe <vollkornaffe@gmail.com>
2025-12-15 15:37:53 +0100

    add test for squishy volumes

---
+++ tests/test_squishy_volumes.py
@@ -0,0 +1,32 @@
+import bpy
+
+from pathlib import Path
+
+from .util import save_failed, round_trip_with_same_external
+
+
+_DIR = Path("tests") / "binary_blend_files" / "squishy_volumes"
+
+
+def test_new_input():
+    path = _DIR / "new_input.blend"
+    try:
+        bpy.ops.wm.open_mainfile(filepath=str(path))
+
+        round_trip_with_same_external(name="Geometry Nodes", is_material=False)
+        round_trip_with_same_external(name="Geometry Nodes.001", is_material=False)
+        round_trip_with_same_external(
+            name="Squishy Volumes Particle", is_material=False
+        )
+
+        round_trip_with_same_external(
+            name="Squishy Volumes Colored Instances", is_material=True
+        )
+        round_trip_with_same_external(
+            name="Squishy Volumes Display UVW", is_material=True
+        )
+
+    except:
+        # store in case of failure for easy debugging
+        save_failed(f"{test_new_input.__name__}")
+        raise
