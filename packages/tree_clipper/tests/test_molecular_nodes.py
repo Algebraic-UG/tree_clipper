71e6f9743bd9f0650ae96e094997ce6f2fee8e94
Vollkornaffe <vollkornaffe@gmail.com>
2025-12-20 22:15:06 +0100

    fix molecular nodes test

--- packages/tree_clipper/tests/test_molecular_nodes.py
+++ packages/tree_clipper/tests/test_molecular_nodes.py
@@ -11,6 +11,14 @@ def test_node_data_file():
     try:
         bpy.ops.wm.open_mainfile(filepath=str(path))
 
+        # https://github.com/Algebraic-UG/tree_clipper/issues/115
+        bpy.data.node_groups["Style Preset 3"].nodes["Group.004"].inputs[
+            2
+        ].default_value = "Instance"  # ty:ignore[unresolved-attribute]
+        bpy.data.node_groups["Style Preset 4"].nodes["Group.011"].inputs[
+            2
+        ].default_value = "Instance"  # ty:ignore[unresolved-attribute]
+
         round_trip(original_name="NodeStorage", is_material=False)
 
         round_trip(original_name="MN Ambient Occlusion", is_material=True)
