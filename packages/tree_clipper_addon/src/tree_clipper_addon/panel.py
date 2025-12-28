d02afd08f335d134955bde74dd0fbf0153800d7d
Vollkornaffe <vollkornaffe@gmail.com>
2025-12-28 22:21:53 +0100

    tidy up more warnings

--- packages/tree_clipper_addon/src/tree_clipper_addon/panel.py
+++ packages/tree_clipper_addon/src/tree_clipper_addon/panel.py
@@ -15,17 +15,17 @@ class SCENE_PT_Tree_Clipper_Panel(bpy.types.Panel):
 
         node_tree = context.space_data.node_tree
         if node_tree is None:
-            self.layout.label(text="No node tree.")
+            self.layout.label(text="No node tree.")  # ty:ignore[possibly-missing-attribute]
             return
 
         is_material = isinstance(context.space_data.id, bpy.types.Material)
         if is_material:
-            name = context.space_data.id.name
+            name = context.space_data.id.name  # ty:ignore[possibly-missing-attribute]
         else:
-            name = context.space_data.node_tree.name
+            name = context.space_data.node_tree.name  # ty:ignore[possibly-missing-attribute]
 
-        export_op = self.layout.operator(SCENE_OT_Tree_Clipper_Export_Prepare.bl_idname)
+        export_op = self.layout.operator(SCENE_OT_Tree_Clipper_Export_Prepare.bl_idname)  # ty:ignore[possibly-missing-attribute]
         export_op.is_material = is_material
         export_op.name = name
 
-        self.layout.operator(SCENE_OT_Tree_Clipper_Import_Prepare.bl_idname)
+        self.layout.operator(SCENE_OT_Tree_Clipper_Import_Prepare.bl_idname)  # ty:ignore[possibly-missing-attribute]
