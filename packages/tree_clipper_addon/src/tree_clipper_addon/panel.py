239be1d557858179dba56d95f9ede3333693070f
Vollkornaffe <vollkornaffe@gmail.com>
2026-01-01 13:35:12 +0100

    display UI in case of no tree but disable export

--- packages/tree_clipper_addon/src/tree_clipper_addon/panel.py
+++ packages/tree_clipper_addon/src/tree_clipper_addon/panel.py
@@ -16,22 +16,24 @@ class SCENE_PT_Tree_Clipper_Panel(bpy.types.Panel):
     def draw(self, context: bpy.types.Context) -> None:
         assert isinstance(context.space_data, bpy.types.SpaceNodeEditor)
 
+        export_col = self.layout.column()  # ty:ignore[possibly-missing-attribute]
+        export_op = export_col.operator(SCENE_OT_Tree_Clipper_Export_Prepare.bl_idname)
+
         node_tree = context.space_data.node_tree
         if node_tree is None:
-            self.layout.label(text="No node tree.")  # ty:ignore[possibly-missing-attribute]
-            return
-
-        is_material = isinstance(context.space_data.id, bpy.types.Material)
-        if is_material:
-            name = context.space_data.id.name  # ty:ignore[possibly-missing-attribute]
+            export_col.enabled = False
         else:
-            name = context.space_data.node_tree.name  # ty:ignore[possibly-missing-attribute]
+            is_material = isinstance(context.space_data.id, bpy.types.Material)
+            if is_material:
+                name = context.space_data.id.name  # ty:ignore[possibly-missing-attribute]
+            else:
+                name = context.space_data.node_tree.name  # ty:ignore[possibly-missing-attribute]
 
-        export_op = self.layout.operator(SCENE_OT_Tree_Clipper_Export_Prepare.bl_idname)  # ty:ignore[possibly-missing-attribute]
-        export_op.is_material = is_material
-        export_op.name = name
+            export_op.is_material = is_material
+            export_op.name = name
 
         self.layout.separator()  # ty:ignore[possibly-missing-attribute]
 
-        self.layout.operator(SCENE_OT_Tree_Clipper_Import_Clipboard_Prepare.bl_idname)  # ty:ignore[possibly-missing-attribute]
-        self.layout.operator(SCENE_OT_Tree_Clipper_Import_File_Prepare.bl_idname)  # ty:ignore[possibly-missing-attribute]
+        import_col = self.layout.column()  # ty:ignore[possibly-missing-attribute]
+        import_col.operator(SCENE_OT_Tree_Clipper_Import_Clipboard_Prepare.bl_idname)
+        import_col.operator(SCENE_OT_Tree_Clipper_Import_File_Prepare.bl_idname)
