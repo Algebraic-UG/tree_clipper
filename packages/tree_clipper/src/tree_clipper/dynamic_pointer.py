90e7a050ffcf3b644928832c275811ddff7e5bdb
Vollkornaffe <vollkornaffe@gmail.com>
2026-07-28 11:52:49 +0200

    fix: load in a temporary text curve to trigger loading default font

--- packages/tree_clipper/src/tree_clipper/dynamic_pointer.py
+++ packages/tree_clipper/src/tree_clipper/dynamic_pointer.py
@@ -111,10 +111,8 @@ def add_all_known_pointer_properties(
         # Otherwise, there might be no font to select
         # https://github.com/Algebraic-UG/tree_clipper/issues/219
         if type_name == "VectorFont" and not bpy.data.fonts:
-            font_path = (
-                Path(bpy.utils.system_resource("DATAFILES")) / "fonts" / "Inter.woff2"
-            )
-            bpy.data.fonts.load(str(font_path), check_existing=True)
+            text_curve = bpy.data.curves.new("Temp Text", type="FONT")
+            bpy.data.curves.remove(text_curve)
 
     # this is needed to display the property
     def get_active_pointer_identifier(self) -> str:
