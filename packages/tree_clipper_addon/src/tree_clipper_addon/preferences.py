7b59e6fee43e58fe36e25090b0152ffa85598a6a
Vollkornaffe <vollkornaffe@gmail.com>
2026-09-02 18:39:07 +0200

    feat: add asset preferences and refactor a bit

--- packages/tree_clipper_addon/src/tree_clipper_addon/preferences.py
+++ packages/tree_clipper_addon/src/tree_clipper_addon/preferences.py
@@ -21,21 +21,46 @@ The default value is somewhat conservative, but not guaranteed to be safe.""",
         default=False,
     )  # type: ignore
 
+    import_as_asset: bpy.props.BoolProperty(
+        name="Import as Asset",
+        description="""Instead of having the imported trees in the current .blend file,
+store the imports in a local asset library.") # type: ignore""",
+        default=True,
+    )  # type :ignore
+
+    asset_directory: bpy.props.StringProperty(
+        name="Asset Directory",
+        description="When importing as asset, store the asset files in this directory.",
+        default=bpy.utils.extension_path_user(
+            __package__,  # ty:ignore[invalid-argument-type]
+            path="assets",
+            create=True,
+        ),
+        subtype="DIR_PATH",
+    )  # ty:ignore[invalid-type-form]
+
     def draw(self, context: bpy.types.Context) -> None:
         self.layout.prop(self, "max_clipboard_megabyte")
         self.layout.prop(self, "show_advanced_options")
+        self.layout.prop(self, "import_as_asset")
+        self.layout.prop(self, "asset_directory")
+
+
+def _get_preferences():
+    return bpy.context.preferences.addons.get(__package__).preferences  # ty:ignore[unresolved-attribute]
 
 
 def get_max_clipboard_bytes():
-    return (
-        1_000_000
-        * bpy.context.preferences.addons.get(  # ty:ignore[possibly-missing-attribute]
-            __package__  # ty:ignore[invalid-argument-type]
-        ).preferences.max_clipboard_megabyte
-    )  # ty:ignore[possibly-missing-attribute]
+    return 1_000_000 * _get_preferences().max_clipboard_megabyte
 
 
 def get_show_advanced_options():
-    return bpy.context.preferences.addons.get(  # ty:ignore[possibly-missing-attribute]
-        __package__  # ty:ignore[invalid-argument-type]
-    ).preferences.show_advanced_options  # ty:ignore[possibly-missing-attribute]
+    return _get_preferences().show_advanced_options
+
+
+def get_import_as_asset():
+    return _get_preferences().import_as_asset
+
+
+def get_asset_directory():
+    return _get_preferences().asset_directory
